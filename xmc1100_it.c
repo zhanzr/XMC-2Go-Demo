bc7413fc40a050d0c4fdedad03a29e1c6172629f
zhanzr <zhanzr@foxmail.com>
2017-10-15 02:17:35 +0800

    first commit

---
+++ xmc1100_it.c
@@ -0,0 +1,155 @@
+#include <XMC1100.h>
+#include <stdio.h>
+
+void HardFault_Handler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void SVC_Handler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void PendSV_Handler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void SCU_0_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void SCU_1_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+                      
+void SCU_2_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+void ERU0_0_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void ERU0_1_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+                   
+void ERU0_2_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void ERU0_3_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+__weak void USIC0_0_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}        
+       
+void USIC0_1_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void USIC0_2_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void USIC0_3_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}        
+       
+void USIC0_4_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void USIC0_5_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void VADC0_C0_0_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}      
+      
+void VADC0_C0_1_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void CCU40_0_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void CCU40_1_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}   
+      
+void CCU40_2_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+}
+
+void CCU40_3_IRQHandler(void)
+{
+	printf(__FUNCTION__);
+	while(1)
+	{;}
+} 
